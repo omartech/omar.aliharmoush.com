fd6c1a3d5a2bc59b69610ab314bb581441db11ac
root <root@omardev.(none)>
2013-10-21 23:09:18 +0400

    my first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+	<title>
+	Omar Ali Ibrahim Harmoush - contacts
+	</title>
+	
+	<meta charset='utf-8'></meta>
+</head>
+
+<body>
+
+<div style='float:left;'>
+<table>
+
+<tr><td><a target='_blank' href="tel:00971501631194"><img src='phone.png' height='50px'></a></td>
+<td><a target='_blank' href='mailto:omar@aliharmoush.com'><img src='email.jpg' height='50px'></a></td></tr>
+<tr><td><a target='_blank' href='mailto:omar.technologies@gmail.com'><img src='gmail.png' height='50px'></a></td>
+<td><a target='_blank' href='http://omar.aliharmoush.com'><img src='web.jpg' height='50px'></a></td></tr>
+<tr><td><a target='_blank' href='http://www.linkedin.com/pub/%D8%B9%D9%85%D8%B1-%D8%A5%D8%A8%D8%B1%D8%A7%D9%87%D9%8A%D9%85/79/836/1ab'><img src='linkedin.png' height='50px'></a></td>
+<td><a target='_blank' href='https://www.github.com/omartech'><img src='github.png' height='50px'></a></td></tr>
+<tr><td><a target='_blank' href='https://twitter.com/OmarThrowsRocks'><img src='twitter.jpg' height='50px'></a></td>
+<td><a target='_blank' href='https://www.facebook.com/omar.align:centertechnologies'><img src='facebook.jpg' height='50px'></a></td></tr>
+
+</table>
+</div>
+
+<div style='float:left; padding:0px; width:60%; '>
+	
+	
+		<h3>
+	Hello, this is my contacts page. Click on the various links to get to where you wanna get to. 
+	If you'd like to know what is my blood group, or how many cavities do I have, please contact the <a href='http://www.nsa.gov'>NSA</a>. They should have more info on the subject.
+		</h3>
+		
+		<h4>
+			The css on this page is childish. It's plain html. I love css. I love php ... But I just can't get the time to work on a 'personal page'. Maybe when I'm on a vacation.
+		</h4>
+	
+</div>
